7f6b3b9cde2df98da75c880e3badf648d5429665
Arti <analegaonkar@gmail.com>
2020-04-14 23:54:34 +0530

    JWT_encode_change

--- AuthenticationMS/security.py
+++ AuthenticationMS/security.py
@@ -18,9 +18,11 @@ import requests
 
 def authenticate(username, password):
     user = requests.get('http://127.0.0.1:5000/user/' + username)
-    if user[0] and safe_str_cmp(user.password, password):
-        return user
+    if user[2].username and safe_str_cmp(user[2].password, password):
+        return user.username
 
 def identity(payload):
-    user_id = payload['identity']
-    return UserModel.find_by_id(user_id)
+    user_id = payload['id']
+    userstatus = requests.get('http://127.0.0.1:5000/user/find_user_by_id/' + user_id)
+    return userstatus
+
--- userManagementMS/app.py
+++ userManagementMS/app.py
@@ -1,8 +1,9 @@
 
-from flask import Flask, request, make_response,jsonify
+from flask import Flask, request, make_response,jsonify,Blueprint
 import models.model as model
-import datetime
-from flask_jwt import JWT, jwt_required
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -13,30 +14,41 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 def hello():
     return {"hello": "user manager"}
 
+@app.route("/user/auth",  methods=['POST'])
+def call_authenticate():
+    data = request.get_json()
+    user = model.UserManagerDB.authenticate(data)
+    if user:
+        return jsonify(user.json()),200
+    else:
+        return None,404
+
+
 @app.route("/user/<string:user>")
 def user_exists(user):
     query_user = model.UserManagerDB.query.filter_by(username=user).first()
     if query_user:
-        return "User exists - true", 201
+        return "User exists - true", 201, query_user
     else:
-        return "User does not exists - false", 401
+        return "User does not exists - false", 401, None
+
+@app.route("/user/find_user_by_id/<int:user_id>")
+def find_user_by_id(user_id):
+    query_user = model.UserManagerDB.query.find_by_id(id=user_id)
+    if query_user:
+        return "true"
+    else:
+        return "false"
 
 @app.route("/user/register", methods=['POST'])
 def register():
     if request.method == 'POST':
         if request.is_json:
             data = request.get_json()
-            new_user = model.UserManagerDB(
-                data['name'],
-                data['username'],
-                data['password'],
-            )
-        response = user_exists(data['username'])
-        if response[1] == 401:
-            new_user.save_to_db()
-            return "saved to record - true", 201
-        else:
-            return "didnt saved to record- already present false", 401
+            user = UserManagerDB(**data)
+            # new_user = model.UserManagerDB( data['name'],data['username'],data['password'], )
+            user.save_to_db()
+            return jsonify(user.to_dict()), 201
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=os.environ.get('PORT'), debug=True)
